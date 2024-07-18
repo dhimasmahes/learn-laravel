c510b52a28470a4e65472b3a5d9c08711e68f690
dhimasmahes <dhimasmahes@gmail.com>
2024-07-18 11:52:45 +0900

    Initiation

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view(
+        'home',
+        ['title' => 'Home Page'],
+    );
+});
+
+Route::get('/about', function () {
+    return view(
+        'about',
+        ['nama' => 'Dhimas', 'title' => 'About'],
+    );
+});
+
+Route::get('/posts', function () {
+    return view(
+        'posts',
+        [
+            'title' => 'Blog',
+            'posts' => Post::all()
+        ]
+        
+    );
+});
+
+Route::get('/posts/{post:slug}', function(Post $post) {
+
+    // $post = Post::find($id);
+
+    return view('post', ['title'=>'Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
+    return view(
+        'contact',
+        ['title' => 'Contact'],
+    );
+});
